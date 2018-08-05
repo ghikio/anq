8bf46109ad6f842d3902d54c513da8efce0a0724
criw <criw@protonmail.com>
2018-08-06 01:43:08 +0200

    Cleaned err_codes.h and updated documentation

--- src/err_codes.h
+++ src/err_codes.h
@@ -8,27 +8,20 @@
 #ifndef ANQ_ERR_CODES_HH
 #define ANQ_ERR_CODES_HH
 
-#define ANQ_OK 0
-
 typedef int ANQ_ERR;
 
-enum s_exit_err_codes {
-	/* command line argument -e or -d has been parsed
-	 * without giving the subsequent service filename. */
-	ANQ_ERR_UNKNOWN_SERVICE = -1,
-	/* argument handler hasn't been able to start */
-	ANQ_ERR_ARGV_HANDLER	= -2,
-	ANQ_ERR_NEED_DELIMITER  = -3, 
-};
+enum anq_err_codes {
+	ANQ_OK = 0,
 
-enum s_return_err_codes {
+	/* Argument handler hasn't been able to start */
+	ANQ_ERR_ARGV_HANDLER = -1,
+
+	/* Given parameter needs a value and it hasn't. */
+	ANQ_ERR_NO_DELIMITER = -2,
+	
 	/* Returned if a malloc() or similar hasn't been able to
 	 * allocate memory */
 	ANQ_ERR_UNALLOCATED_STRUCT = -100,
-	/* Returned when s_argv_handler.argc "argv_handler.h" has
-	 * exceeded ARGV_SIZE "argv_handler.h" */
-	ANQ_ERR_MAX_ARGS_EXCEEDED = -101,
-	ANQ_ERR_NO_DELIMITER = -102,
 };
 
 #endif
